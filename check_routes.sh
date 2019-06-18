bedc3bb02bf4821a1e8754f433bb86cbf207f624
daichi-kimura <daichi.kimura@g.softbank.co.jp>
2019-06-18 15:57:08 +0900

    Remove set env. Will be assed from external (concourse).

--- check_routes.sh
+++ check_routes.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-export BOSH_CLIENT= <uaa_bosh_client>
-export BOSH_CLIENT_SECRET= <uaa_bosh_secret>
-export BOSH_CA_CERT= <path_to_ca_cert>
-export BOSH_ENVIRONMENT= <ip_of_bosh_director>
-export APP_NAME= <app_name>
-export DEPLOYMENT_NAME= <dep_name>
-export ROUTER_INSTANCE_NUM= <number_of_gorouter>
+# export BOSH_CLIENT= <uaa_bosh_client>
+# export BOSH_CLIENT_SECRET= <uaa_bosh_secret>
+# export BOSH_CA_CERT= <path_to_ca_cert>
+# export BOSH_ENVIRONMENT= <ip_of_bosh_director>
+# export APP_NAME= <app_name>
+# export DEPLOYMENT_NAME= <dep_name>
+# export ROUTER_INSTANCE_NUM= <number_of_gorouter>
 
 bosh -d $DEPLOYMENT_NAME ssh diego_cell/0 -c "source /var/vcap/jobs/cfdot/bin/setup; cfdot actual-lrp-groups > /tmp/cfdot.json"
 bosh -d $DEPLOYMENT_NAME scp diego_cell/0:/tmp/cfdot.json /tmp/cfdot.json
